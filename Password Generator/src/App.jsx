7e172e7baa018f4be5c166740ad869d49ad7d110
Shashank Devadiga <beast.steam002@gmail.com>
2024-01-25 00:07:34 +0530

    Password Generator

---
+++ Password Generator/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <>
+    <h1 className="text-4xl text-center text-white">Password Generator</h1>
+    </>
+  )
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # React Practice Projects
  
 ## 1. Background Changer: https://backgroundchanger-sigma.vercel.app/
+## 2. Password Generator: 
