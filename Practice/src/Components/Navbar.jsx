df76fa73c22621e3743828407fd96cf6acd5699d
Shashank Devadiga <beast.steam002@gmail.com>
2024-05-01 19:20:37 +0530

    Added react-icons package and updated dependencies in package.json and package-lock.json

--- Practice/src/Components/Navbar.jsx
+++ Practice/src/Components/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
   return (
     <nav className=' w-full h-20 bg-sky-500 x-6 py-2 flex justify-between px-12 items-center text-white'>
-        <h1 className='font-bold text-xl '>Brand Logo</h1>
+        <h1 className='font-bold text-xl '>BeastHub ❤️</h1>
 
         <ul className='flex items-center gap-6 max-sm:hidden'>
-            <a href=""><h3 className='tetx-lg font-semibold'>Home</h3></a>
-            <a href=""><h3 className='tetx-lg font-semibold'>About</h3></a>
-            <a href=""><h3 className='tetx-lg font-semibold'>Services</h3></a>
-            <a href=""><h3 className='tetx-lg font-semibold'>Contact Us</h3></a>
+            <a href=""><h3 className='text-lg font-semibold'>Home</h3></a>
+            <a href=""><h3 className='text-lg font-semibold'>About</h3></a>
+            <a href=""><h3 className='text-lg font-semibold'>Services</h3></a>
+            <a href=""><h3 className='text-lg font-semibold'>Contact Us</h3></a>
         </ul>
+
+        <h1 className='sm:hidden  font-bold text-2xl'><IoMenu /></h1>
     </nav>
   )
 }
